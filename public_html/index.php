cfef6a9e42b11450c564addc1edb89d9b59ba61c
Minero20 <pisit.namsiri@gmail.com>
2022-12-19 18:15:13 +0700

    update

--- public_html/index.php
+++ public_html/index.php
@@ -7,8 +7,25 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     </head>
     <body>
-        <h1>Welcome to my home page!</h1>
-        <?php include 'vars.php';
-        echo "I have a $color $car.";?>
+        <?php
+        $number = $_POST['number'];
+        if(isset($_POST['submit'])) 
+        {
+            include 'vars.php';
+            $analysis = new Time();
+            $analysis->set_number($number);
+        }
+        if (isset($_POST['clear'])) 
+        {
+            $number = "";
+        } 
+        ?>   
+        <h2> HTML Forms Input and Display in PHP </h2>
+        <form method="post">
+        <p>number: <input type="number" name="number" value="<?php echo $number; ?>" autofocus> &nbsp;&nbsp;&nbsp;
+        <input type="submit" name="submit" value="Submit" /> &nbsp;&nbsp;&nbsp;
+        <input type="submit" name="clear" value="Clear" />
+        </form>
+        <?php $analysis->Display_Time(); ?>
     </body>
 </html>
--- public_html/vars.php
+++ public_html/vars.php
@@ -1,4 +1,24 @@
 <?php
-$color='red';
-$car='BMW';   
+class Time 
+{
+  // Properties
+  private $number;
+  // Methods
+  function set_number($n) 
+  {
+    $this->number = $n;
+  }
+  function get_number() 
+  {
+    return $this->number;
+  }
+  function Display_Time()
+  {
+    echo "<br/> Display Results <br/>";
+    for ($x = 1; $x <= 12; $x++) 
+    {
+        echo "$this->number * $x : " . $this->number * $x ."<br/>";
+    }
+  }
+}
 ?>
